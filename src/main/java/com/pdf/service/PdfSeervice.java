8721c40050ab54334fcc8468ebf17b1be6cfa4f7
pengweidong <dI226Jp:t6dq>
2020-09-24 19:52:07 +0800

    提交

--- src/main/java/com/pdf/service/PdfSeervice.java
+++ src/main/java/com/pdf/service/PdfSeervice.java
@@ -5,5 +5,5 @@ import com.pdf.pojo.FileContent;
 public interface PdfSeervice {
     void saveFileInfo(FileContent fileContent) throws Exception;
 
-    FileContent getFileContent(FileContent fileContent);
+    FileContent getFileContent(FileContent fileContent) throws Exception;
 }
