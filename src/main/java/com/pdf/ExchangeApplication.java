9482ac408456b203e0930cfd4de31d1d3dab2535
pengweidong <dI226Jp:t6dq>
2020-09-27 19:27:27 +0800

    提交

--- src/main/java/com/pdf/ExchangeApplication.java
+++ src/main/java/com/pdf/ExchangeApplication.java
@@ -3,12 +3,19 @@ package com.pdf;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 @MapperScan(value = "com.pdf.dao")
-public class ExchangeApplication {
+public class ExchangeApplication extends SpringBootServletInitializer {
     public static void main(String[] args) {
         SpringApplication.run(ExchangeApplication.class, args);
     }
 
+    @Override
+    protected SpringApplicationBuilder configure(
+            SpringApplicationBuilder builder) {
+        return builder.sources(this.getClass());
+    }
 }
